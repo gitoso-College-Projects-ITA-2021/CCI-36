6e4bfa066416df3713afc9dcaaa9175b99774813
Gianluigi Dal Toso <gianluigi.toso@gmail.com>
2020-06-24 23:19:56 -0300

    Add music to zip script

--- public/zipar_ativ2.sh
+++ public/zipar_ativ2.sh
@@ -3,6 +3,6 @@ mkdir ../zips
 rm -f ../zips/atividade2.zip
 mv index.html realindex.html
 mv atividade2.html index.html
-zip -r ../zips/atividade2.zip src/atividade2/* lib/* assets/boat/* assets/boat2/* assets/terrain_textures/* assets/arvore* assets/waternormals.jpg index.html *.css
+zip -r ../zips/atividade2.zip index.html src/atividade2/* lib/* assets/boat/* assets/boat2/* assets/terrain_textures/* assets/arvore* assets/waternormals.jpg *.css music/*
 mv index.html atividade2.html
 mv realindex.html index.html
