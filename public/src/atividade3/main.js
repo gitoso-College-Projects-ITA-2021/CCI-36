6bc568aa8247271361a767ac38c5e69e6ef83db0
Raphael de Vasconcelos Nascimento <rapharvn@gmail.com>
2020-07-19 22:27:36 -0300

    Adicionado grid pra melhor visualizaçao

--- public/src/atividade3/main.js
+++ public/src/atividade3/main.js
@@ -1,6 +1,7 @@
 // Three.js setup
 var renderer = new THREE.WebGLRenderer({
     canvas:document.getElementById("main-canvas"),
+    alpha:true,
     antialias:true
 })
 renderer.setSize(window.innerWidth, window.innerHeight)
@@ -21,16 +22,19 @@ scene.add( light );
 var size = 100;
 var divisions = 10;
 var gridHelper = new THREE.GridHelper(size, divisions);
-scene.add(gridHelper);
+//scene.add(gridHelper);
 
 var gx = 10;
 var gy = 15;
 var gz = 10;
 var grid_size = gx * gy * gz;
 
+
 function grid_idx(x, y, z) {
     return x + y * gx + z * gx * gy;
 }
+
+
 
 var grid_cubes = new Array(grid_size);
 for (var x = 0; x < gx * 10; x += 10) {
@@ -39,16 +43,86 @@ for (var x = 0; x < gx * 10; x += 10) {
             var idx = grid_idx(x, y , z);
             var geom = new THREE.BoxGeometry(9.8, 9.8, 9.8);
             var mat = new THREE.MeshPhongMaterial( {color: 0x00ff00} );
+
             grid_cubes[idx] = new THREE.Mesh(geom, mat);
             grid_cubes[idx].position.x = x;
             grid_cubes[idx].position.y = y;
             grid_cubes[idx].position.z = -z;
+
             scene.add(grid_cubes[idx]);
             grid_cubes[idx].visible = false;
         }
     }
 }
+var geom = new THREE.BoxGeometry((gx - 1) * 10, (gy - 1) * 10, (gz - 1) * 10);
+var mat = new THREE.MeshPhongMaterial( {color: 0x000fff, opacity: 0.1, transparent: true} );
 
+grid_cube = new THREE.Mesh(geom, mat);
+grid_cube.position.x = (gx - 1)*10/2;
+grid_cube.position.y = (gy - 1)*10/2;
+grid_cube.position.z = -(gz - 1)*10/2;
+scene.add(grid_cube);
+
+
+// https://discourse.threejs.org/t/3d-grid-of-lines/3850
+let xSize = gx;
+let ySize = gy;
+let zSize = gz;
+let n = xSize * ySize * zSize;
+
+let geometry = new THREE.BufferGeometry();
+
+function mapTo3D(i) {
+	let z = Math.floor(i / (xSize * ySize));
+	i -= z * xSize * ySize;
+	let y = Math.floor(i / xSize);
+	let x = i % xSize;
+	return { x: x, y: y, z: z };
+}
+
+function mapFrom3D(x, y, z) {
+	return x + y * xSize + z * xSize * ySize;
+}
+
+let positions = [];
+//for (let i = 0; i < n; i++) {
+	//let p = mapTo3D(i);
+	//positions.push(p.x);
+	//positions.push(p.y);
+	//positions.push(p.z);
+//}
+for (var x = 0; x < gx * 10; x += 10) {
+    for (var y = 0; y < gy * 10; y += 10) {
+        for (var z = 0; z < gz * 10; z += 10) {
+            positions.push(x);
+            positions.push(y);
+            positions.push(-z);
+        }
+    }
+}
+let positionAttribute = new THREE.Float32BufferAttribute(positions, 3);
+geometry.addAttribute("position", positionAttribute);
+
+let indexPairs = [];
+for (let i = 0; i < n; i++) {
+	let p = mapTo3D(i);
+	if (p.x + 1 < xSize) {
+		indexPairs.push(i);
+		indexPairs.push(mapFrom3D(p.x + 1, p.y, p.z));
+	}
+	if (p.y + 1 < ySize) {
+		indexPairs.push(i);
+		indexPairs.push(mapFrom3D(p.x, p.y + 1, p.z));
+	}
+	if (p.z + 1 < zSize) {
+		indexPairs.push(i);
+		indexPairs.push(mapFrom3D(p.x, p.y, p.z + 1));
+	}
+}
+geometry.setIndex(indexPairs);
+let lines = new THREE.LineSegments(geometry, new THREE.LineBasicMaterial( {opacity : 0.1, transparent: true} ));
+scene.add(lines);
+// ----
 
 var px = 3;
 var py = 3;
@@ -64,7 +138,7 @@ p_cubes[p_idx(0, 0, 0)] = true;
 p_cubes[p_idx(1, 0, 0)] = true;
 p_cubes[p_idx(0, 1, 0)] = true;
 p_cubes[p_idx(0, 0, 1)] = true;
-var p_pos = new THREE.Vector3(5, 10, 5);
+var p_pos = new THREE.Vector3(2, 13, 2);
 for (var x = 0; x < px; x++) {
     for (var y = 0; y < py; y++) {
         for (var z = 0; z < pz; z++) {
@@ -122,13 +196,16 @@ function animate() {
                 if (p_cubes[p_idx(x, y, z)] == true) {
                     var pold = new THREE.Vector3(x, y, z);
                     pold.applyAxisAngle(new THREE.Vector3(0, 1, 0), rot_old);
+                    pold.x = Math.round(pold.x);
+                    pold.y = Math.round(pold.y);
+                    pold.z = Math.round(pold.z);
                     g_idx = grid_idx((px_old + pold.x) * 10, (py_old + pold.y) * 10, (pz_old + pold.z) * 10);
                     grid_cubes[g_idx].visible = false;
-                    //p.x = Math.ceil(p.x);
-                    //p.y = Math.ceil(p.y);
-                    //p.z = Math.ceil(p.z);
                     var p= new THREE.Vector3(x, y, z);
                     p.applyAxisAngle(new THREE.Vector3(0, 1, 0), rot);
+                    p.x = Math.round(p.x);
+                    p.y = Math.round(p.y);
+                    p.z = Math.round(p.z);
                     g_idx = grid_idx((p_pos.x + p.x) * 10, (p_pos.y + p.y) * 10, (p_pos.z + p.z) * 10);
                     grid_cubes[g_idx].visible = true;
                 }
